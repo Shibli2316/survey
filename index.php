8d7a3bb886f5ce39a8684719a510c880cfd8df65
Shibli <ahmedrazashibli@gmail.com>
2023-07-16 12:11:55 +0530

    initial website pages

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    click here to <a href="voulenteer/index.php">Login</a> as a voulenteer
+    or click <a href="arsir/index.php">here</a>
+</body>
+</html>
